7056b434ba9c1f4a1e9b56f60671f3e69a05fa54
Waqar Qureshi <bionic.waqar@gmail.com>
2023-08-21 23:42:39 -0400

    gradle update

--- adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import geolocator_apple
 import path_provider_foundation
 import share_plus
@@ -12,6 +13,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
