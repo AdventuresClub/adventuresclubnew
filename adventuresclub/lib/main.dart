76ab4e651d4926249d6f3b06fdc1dd3d9b3099f8
Waqar Qureshi <20317162+bionicwaqar@users.noreply.github.com>
2024-07-24 14:07:09 -0400

    notifications update

--- adventuresclub/lib/main.dart
+++ adventuresclub/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:adventuresclub/provider/services_provider.dart';
 import 'package:adventuresclub/splashScreen/splash_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -13,9 +14,6 @@ import 'package:intl/date_symbol_data_local.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   initializeDateFormatting().then(
     (_) => runApp(
       MultiProvider(
@@ -38,12 +36,53 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future getAppFuture;
+  @override
+  void initState() {
+    getAppFuture = getApp();
+
+    super.initState();
+  }
+
+  Future<void> registerFCM() async {
+    await FirebaseMessaging.instance.requestPermission();
+    final fcmToken = await FirebaseMessaging.instance.getToken(
+        vapidKey:
+            "BEr0HbHx_pAg1PMPbqHuA2g0hQrHtbvsM5cNfxMThTHvnvcH01-Z8MnBo-qyDR0LvRPi2fvb_3WVWf4T2rlLOhg");
+    if (fcmToken != null) {
+      setFCMToken(fcmToken);
+    }
+    FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
+      setFCMToken(fcmToken);
+    }).onError((err) {});
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      if (notification != null) {
+        Constants.showMessage(
+            context, "${notification.title}: ${notification.body}");
+        debugPrint('onMessage: ${notification.toString()}');
+      }
+    });
+  }
+
   Future<void> getApp() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await registerFCM();
+
     await Constants.getPrefs();
   }
+
+  void setFCMToken(String fcmToken) async {}
 
   // This widget is the root of your application.
   @override
--- adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import device_info_plus
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import firebase_messaging
 import firebase_storage
 import geolocator_apple
 import shared_preferences_foundation
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
