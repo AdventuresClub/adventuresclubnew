5227fb374c8038625cdc3196cf86065b3337f8e7
Hamzacyber15 <63361461+Hamzacyber15@users.noreply.github.com>
2024-08-12 21:49:05 +0400

    hamza

--- adventuresclub/lib/constants.dart
+++ adventuresclub/lib/constants.dart
@@ -98,7 +98,7 @@ class Constants {
   static Map mapAimedFilter = {};
   static bool partnerRequest = false;
   static String lastestVersion = "";
-  static double currentVersion = 3.3;
+  static double currentVersion = 3.4;
   static List<SectorFilterModel> filterSectors = [];
   static List<CategoryFilterModel> categoryFilter = [];
   static List<ServiceTypeFilterModel> serviceFilter = [];
