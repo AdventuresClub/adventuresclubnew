66ec3f560aaa05b4bb83b4f9c3574db510ea3afb
Waqar Qureshi <20317162+bionicwaqar@users.noreply.github.com>
2024-11-03 12:50:10 -0500

    update

--- adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ adventuresclub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,6 +19,7 @@ import printing
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -35,4 +36,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
