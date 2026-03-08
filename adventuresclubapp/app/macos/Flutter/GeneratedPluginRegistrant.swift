230e166578b3c8cbe2beed6538b14f120cb3fca6
Waqar Qureshi <20317162+bionicwaqar@users.noreply.github.com>
2026-03-08 15:10:12 -0400

    update

--- adventuresclubapp/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ adventuresclubapp/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,6 @@ import flutter_local_notifications
 import geolocator_apple
 import open_file_mac
 import package_info_plus
-import path_provider_foundation
 import printing
 import shared_preferences_foundation
 import sqflite_darwin
@@ -37,7 +36,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
